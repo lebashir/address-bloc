3d93cf72e65726653f300d76d04cb04f3402c9d3
lebashir <bashir.abihabib@gmail.com>
2017-03-21 14:22:50 -0500

    completed assignment-5-models

--- models/address_book.rb
+++ models/address_book.rb
@@ -5,6 +5,16 @@ class AddressBook
     @entries = []
   end
 
+  def remove_entry(name, phone_number, email)
+    entry_to_delete = nil
+    entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        entry_to_delete = entry
+      end
+    end
+    entries.delete(entry_to_delete)
+  end
+
 def add_entry(name, phone_number, email)
 
   index = 0
